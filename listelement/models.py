c80db31f25a9f05b32362dad1b932f8a3967be47
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-06-18 11:45:18 -0500

    Configuración de la función __str__

--- listelement/models.py
+++ listelement/models.py
@@ -5,10 +5,15 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
 
 class Type(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
 class Element(models.Model):
     title = models.CharField(max_length=255)
@@ -16,3 +21,6 @@ class Element(models.Model):
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
