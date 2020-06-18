005b567f69ef49d211aa06215eb4b45e48dd8135
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-06-18 15:03:11 -0500

    Personalizando los metodos de la REST Api

--- listelement/viewsets.py
+++ listelement/viewsets.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Element, Category, Type
 from .serializer import ElementSerializer, CategorySerializer, TypeSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
 class ElementViewSet(viewsets.ModelViewSet):
     queryset = Element.objects.all()
@@ -9,6 +12,18 @@ class ElementViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+   
+    '''def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Category.objects.all()
+        category = get_object_or_404(queryset, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)'''
 
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
