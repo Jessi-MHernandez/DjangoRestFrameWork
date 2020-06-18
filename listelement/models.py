63cbf96e4e82f2aeb2114d60f697de8467c27c6d
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-06-18 11:11:34 -0500

    Creando modelos iniciales

--- listelement/models.py
+++ listelement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+
+
+class Type(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+
+class Element(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
